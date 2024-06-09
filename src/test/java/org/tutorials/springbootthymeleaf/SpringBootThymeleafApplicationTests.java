c8969605dac19e19025d8600ab96fce6e1d3d040
SOVANNARO <sovannaro066@gmail.com>
2024-06-09 23:11:57 +0700

    init thymeleaf

---
+++ src/test/java/org/tutorials/springbootthymeleaf/SpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorials.springbootthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
